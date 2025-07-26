36f41b062c1b7b1edc84d3e0e9ebe9b11f4e44cd
lucyotoole <lucia@uname.net>
2025-07-26 13:06:03 -0400

    Adding line jump threshold to canny edge algorithm

--- horizon_finder/hori_finder_canny_edge.py
+++ horizon_finder/hori_finder_canny_edge.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-test_landscape_image = 'road_asphalt_highway_mountain_tree-61355.jpg'
+output_file_name_prefix = "H_"
+
+test_landscape_image = "mountains-fjord_G4EWW6PIHV.jpg"
+#'road_asphalt_highway_mountain_tree-61355.jpg'
 
 # Canny Edge: https://docs.opencv.org/4.x/da/d22/tutorial_py_canny.html
 
@@ -16,17 +19,40 @@ height = len(edges)
 width = len(edges[0])
 horizon_line = [-1 for _ in range (0, width)]
 
-for i, row in enumerate(edges):
-    curr_height = height - i # count down from top
-    for j, pixel in enumerate(row):
-        if horizon_line[j] == -1 and pixel != 0:
-            horizon_line[j] = curr_height
+line_jump_thres = 50
+
+prev_hori_pixel = -1
+for col in range(0, width):
+    for i in range(0, height):
+        curr_height = height - i
+        pixel = edges[i][col]
+        print(str(curr_height) + ", " + str(col) + " " + str(pixel))
+        if pixel != 0: # there is an edge
+            # determine if this point is close enough to prev edge
+            if prev_hori_pixel > -1:
+                diff = abs(prev_hori_pixel - curr_height)
+                print(str(curr_height) + " " + str(diff))
+                if (diff > line_jump_thres):
+                    print(str(curr_height) + " " + str(diff))
+                    continue
+            horizon_line[col] = curr_height
+            prev_hori_pixel = curr_height
+            print(edges.shape)
+            break # look at next column
 
 x = [i for i in range (1, width+1)]
 y = horizon_line
 
+# just conney edge
+plt.imshow(edges,cmap = 'gray')
+#plt.xticks([]), plt.yticks([])
+plt.title("Canny Edge")
+
+plt.savefig(output_file_name_prefix + "canny_edge.jpg")
+
 
 # overlay
+plt.clf()
 overlay_y = [-1*val + height for val in horizon_line]
 
 plt.imshow(edges,cmap = 'gray')
@@ -34,22 +60,21 @@ plt.imshow(edges,cmap = 'gray')
 plt.plot(x, overlay_y, color='pink', linewidth=2)  # Plot points connected by line, markers on points
 plt.title("Canny Edge Overlay")
 
-plt.savefig("canny_edge_overlay.jpg")
+plt.savefig(output_file_name_prefix + "canny_edge_overlay.jpg")
 
 # graph
 plt.clf()
-
 plt.plot(x, y, color='blue', lw=2)  # Plot points connected by line, markers on points
 plt.title("Canny Edge XY Plot")
 plt.xlim(0, width)
 plt.ylim(0, height)
 
-plt.savefig("canny_edge_xy_plot.jpg")
+plt.savefig(output_file_name_prefix + "canny_edge_xy_plot.jpg")
 
 # Saving horizon data as csv file
 import csv
 
-with open ("canny_edge_horiz_data.csv", "w+") as file:
+with open (output_file_name_prefix + "canny_edge_horiz_data.csv", "w+") as file:
     writer = csv.writer(file)
     print(horizon_line)
     for val in horizon_line:
---
+++ horizon_finder/hori_finder_image_seg.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+output_file_name_prefix = "C_"
+
+test_landscape_image = "mountains-fjord_G4EWW6PIHV.jpg"
+
+# Image Segmentation: https://docs.opencv.org/4.x/d3/db4/tutorial_py_watershed.html
+
+# Image Segmentation test:
+img = cv.imread(test_landscape_image)
+assert img is not None, "file could not be read, check with os.path.exists()"
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+plt.imshow(thresh,cmap = 'gray')
+#plt.xticks([]), plt.yticks([])
+plt.savefig(output_file_name_prefix + "image_seg_test.jpg")
